9d13c06a29e82cecb0d8c0a206a536b20ddacd82
Hayley Swimelar <hswimelar@gmail.com>
2015-03-24 18:07:28 -0700

    clean up whitespace

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,14 +1,13 @@
 class Board
-
   attr_accessor :current_bord
-  
+ 
   def initialize
     @current_bord = [%w(_ _ _), %w(_ _ _), %w(_ _ _)]
   end
 
   def print_board
-    @current_bord.each do |a| 
-      print a.join(' '), "\n" 
+    @current_bord.each do |a|
+      print a.join(' '), "\n"
     end
   end
 
@@ -16,12 +15,26 @@ class Board
   end
 
   def start_round
+    puts 'Welcome to Tic Tac Toe!'
+
+    puts 'Who is our first player?'
+    player1 = Player.new(gets.chomp)
+
+    puts 'Who is our second player?'
+    player2 = Player.new(gets.chomp)
+    print_board
   end
-  
+
   def start_turn
   end
 
   class Player
+    attr_accessor :name, :score
+
+    def initialize
+      @score = 0
+    end
+
     def place_mark
     end
 
@@ -30,6 +43,9 @@ class Board
 
     def score
     end
-
   end
 end
+
+board = Board.new
+
+board.start_round
