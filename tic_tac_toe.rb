f29d7390e6570b0f536880e29f6956e00a872d59
Hayley Swimelar <hswimelar@gmail.com>
2015-03-24 18:56:45 -0700

    add ability for players to set marks

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,13 +1,13 @@
 class Board
-  attr_accessor :current_bord, :number_of_turns
+  attr_accessor :current_board, :number_of_turns
 
   def initialize
-    @current_bord = [%w(_ _ _), %w(_ _ _), %w(_ _ _)]
+    @current_board = [%w(_ _ _), %w(_ _ _), %w(_ _ _)]
     @number_of_turns = 0
   end
 
   def print_board
-    @current_bord.each do |a|
+    @current_board.each do |a|
       print a.join(' '), "\n"
     end
   end
@@ -20,14 +20,18 @@ class Board
     puts 'Welcome to Tic Tac Toe!'
 
     puts 'Who is our first player?'
-    @player1 = Player.new(gets.chomp)
+    @player1 = Player.new(gets.chomp, '#', self)
 
     puts 'Who is our second player?'
-    @player2 = Player.new(gets.chomp)
+    @player2 = Player.new(gets.chomp, 'O', self)
 
     puts "#{@player1.name}, #{@player2.name}. Are you ready!?"
 
     start_turn
+  end
+
+  def set_board(collumn, row, mark)
+    @current_board[collumn][row] = mark
   end
 
   def start_turn
@@ -44,18 +48,27 @@ class Board
   end
 
   class Player
-    attr_accessor :name, :score
+    attr_accessor :name, :score, :mark, :board
 
-    def initialize(name)
+    def initialize(name, mark, board)
       @name = name
       @score = 0
+      @mark = mark
+      @board = board
     end
 
     def place_mark
-      puts "I'm a dummy method!"
+      puts "#{@name}, what row do you wish to place your mark on? \n1, 2, or 3?"
+      row = gets.chomp.to_i - 1
+
+      puts "What collumn do you wish to place your mark on? \n1, 2, or 3?"
+      collumn = gets.chomp.to_i - 1
+
+      @board.set_board(row, collumn, @mark)
     end
 
     def valid_move?
+      true
     end
 
     def score
