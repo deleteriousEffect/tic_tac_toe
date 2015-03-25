0034f6a2ed2944fda173049f02d2acb722134720
Hayley Swimelar <hswimelar@gmail.com>
2015-03-24 17:38:07 -0700

    add empty methods to classes

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,4 +1,25 @@
 class Board
+  def print_board
+  end
+
+  def victory?
+  end
+
+  def start_round
+  end
+  
+  def start_turn
+  end
+
   class Player
+    def place_mark
+    end
+
+    def valid_move?
+    end
+
+    def score
+    end
+
   end
 end
