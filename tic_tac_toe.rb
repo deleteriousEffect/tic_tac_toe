54a24b28d5b0fbfbd8c87f0845f17098a72850b7
Hayley Swimelar <hswimelar@gmail.com>
2015-03-24 18:37:03 -0700

    add basic game loop

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,8 +1,9 @@
 class Board
-  attr_accessor :current_bord
+  attr_accessor :current_bord, :number_of_turns
 
   def initialize
     @current_bord = [%w(_ _ _), %w(_ _ _), %w(_ _ _)]
+    @number_of_turns = 0
   end
 
   def print_board
@@ -12,23 +13,34 @@ class Board
   end
 
   def victory?
+    false
   end
 
   def start_round
     puts 'Welcome to Tic Tac Toe!'
 
     puts 'Who is our first player?'
-    player1 = Player.new(gets.chomp)
+    @player1 = Player.new(gets.chomp)
 
     puts 'Who is our second player?'
-    player2 = Player.new(gets.chomp)
+    @player2 = Player.new(gets.chomp)
 
-    puts "#{player1.name}, #{player2.name}. Are you ready!?"
+    puts "#{@player1.name}, #{@player2.name}. Are you ready!?"
 
     start_turn
   end
 
   def start_turn
+    loop do
+      print_board
+      break if victory?
+      if @number_of_turns.even?
+        @player1.place_mark
+      else
+        @player2.place_mark
+      end
+      @number_of_turns += 1
+    end
   end
 
   class Player
@@ -40,6 +52,7 @@ class Board
     end
 
     def place_mark
+      puts "I'm a dummy method!"
     end
 
     def valid_move?
